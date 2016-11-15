e9d6d672dc3bc529ad8d2ea20428f906ee42aae8
Gregori Kanatzidis <kanatzidisg@gmail.com>
2016-11-14 19:43:40 -0600

    cleaning up

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 const electron = require('electron')
 const path = require('path');
 const spawn = require('child_process').spawn;
+const { checkForUpdates } = require('geiger-autoupdate');
+
 // Module to control application life.
 const app = electron.app
 // Module to create native browser window.
@@ -44,11 +46,10 @@ function createWindow () {
   mainWindow.loadURL(`file://${__dirname}/index.html`)
 
   // Open the DevTools.
-  mainWindow.webContents.openDevTools()
+  //mainWindow.webContents.openDevTools()
 
   setTimeout(() => {
-const { checkForUpdates } = require('geiger-autoupdate');
-  checkForUpdates();
+    checkForUpdates();
   }, 1000);
   // Emitted when the window is closed.
   mainWindow.on('closed', function () {
